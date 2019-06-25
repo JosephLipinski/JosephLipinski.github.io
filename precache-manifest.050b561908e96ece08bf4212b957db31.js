b5527020ccbafd237976ffe1ba7f7d54c1915dac
Joseph Lipinski <lipinskijoseph@gmail.com>
2019-06-25 18:43:14 -0400

    Updates

---
+++ precache-manifest.050b561908e96ece08bf4212b957db31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8440b57be09132f6b29901fc2bfacfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ab9ed6def79e041aa6",
+    "url": "/static/css/main.b7c8975c.chunk.css"
+  },
+  {
+    "revision": "462a6efd266dee9b0528",
+    "url": "/static/js/2.bf7c5713.chunk.js"
+  },
+  {
+    "revision": "90ab9ed6def79e041aa6",
+    "url": "/static/js/main.3bb00476.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
